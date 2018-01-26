a1589840b1b0b3e2bc1f312883262140f8efe434
Blandine Mallat <blandinemallat@MacBook-Pro-de-Blandine.local>
2018-01-26 14:59:49 +0100

    Exercice 1

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let chalk = require('chalk');
-
+var express = require('express');
+//var app = express();
 const app = require('./src/app');
 
 // port
 const port = 3000;
-
+app.listen(port);
 // start server here
 console.log(chalk.green('Hello web server'));
