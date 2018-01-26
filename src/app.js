3ca5f3b2319663f40e1495622f3ee449924eb178
Blandine Mallat <blandinemallat@MacBook-Pro-de-Blandine.local>
2018-01-26 15:26:01 +0100

    Exercice 4

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 //const app = '... please declare your Express web server here';
 var express = require('express');
 const app = express();
+var fs = require('fs');
+  var numbers
 app.get('/bingo', function(req, res){
-  res.end("Bingo")
+  fs.readFile("src/numbers.txt", "utf8",  function(e, data) {
+    if (e) throw e;
+    var temp = data.split("\n");
+    temp.splice(-1,1)
+    var result = temp.join(', ')
+    res.end(result);
+  })
 })
 module.exports = app;
