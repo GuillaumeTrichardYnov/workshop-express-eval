020453e4d1aab7220e8965b3a6db228260370f3e
Blandine Mallat <blandinemallat@MacBook-Pro-de-Blandine.local>
2018-01-26 15:03:08 +0100

    Exercice 3

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 //const app = '... please declare your Express web server here';
 var express = require('express');
 const app = express();
+app.get('/bingo', function(req, res){
+  res.end("Bingo")
+})
 module.exports = app;
